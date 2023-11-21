a72ff18de86a7db3daf4db137ab7910b2ef94059
Saifullah Khan <135647515+SaiFUllaH-KhaN1@users.noreply.github.com>
2023-11-21 12:49:48 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install portaudio19-dev
-sudo apt-get update
-sudo apt-get install -y portaudio19-dev
+apt-get update
+apt-get install -y portaudio19-dev
 
 # Install Python dependencies
 pip install -r requirements.txt
